87dcffd9b4fbd6c7fb11448c333e88ecd0bec92d
Thiago Deserto <thiago@thiagodeserto.com>
2019-05-01 19:13:00 -0400

    Moved the scripts to inside of the unity project. Created two new prefabs to use as samples. Created a new scriptableObject with the data for two sample ids. Setup the sample scene with the basic data Instance.

---
+++ UnityProject/Procedural Unity/Assets/Scripts/HoudiniData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HoudiniData", menuName = "Houdini/Data", order = 1)]
+public class HoudiniData : ScriptableObject
+{
+	[System.Serializable]
+	public class Entry
+	{
+		public int nodeType;
+		public Quaternion orient;
+		public Vector3 P;
+		public Vector3 scale;
+	}
+
+	public Entry[] _entries;
+}
